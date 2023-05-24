f1f7f916194240c3b2ce0bf129729a6584821a00
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 15:42:43 +0530

    Update Content

--- config-server/src/main/java/lk/ijse/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/lk/ijse/configserver/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package lk.ijse.configserver;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -14,4 +15,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class);
+    }
+
 }
