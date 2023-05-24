a4f1052260b3f7edad39250c8a219ea346f782e5
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 15:31:08 +0530

    Update Content

---
+++ api-gateway/src/main/java/lk/ijse/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package lk.ijse;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @PACKAGE_NAME : lk.ijse
+ * @PROJECT_NAME : SpringBoot-POS_Microservice-BackEnd
+ * @Date : 5/24/2023
+ * @Time : 3:30 PM
+ * @since : 0.1.0
+ **/
+public class ApiGatewayApplication {
+}
