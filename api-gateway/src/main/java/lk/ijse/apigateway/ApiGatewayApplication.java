32e368399b5cf1b8276e8d375452f1c1f3ffb86b
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 15:35:12 +0530

    Update Content

--- api-gateway/src/main/java/lk/ijse/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/lk/ijse/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package lk.ijse;
+package lk.ijse.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author : Nimesh Piyumantha
- * @PACKAGE_NAME : lk.ijse
+ * @PACKAGE_NAME : lk.ijse.apigateway
  * @PROJECT_NAME : SpringBoot-POS_Microservice-BackEnd
  * @Date : 5/24/2023
  * @Time : 3:30 PM
