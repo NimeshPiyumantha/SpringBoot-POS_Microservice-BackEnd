67a01ab3830415ea46b79b39a187e5370ffc4cd4
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 16:15:54 +0530

    Update Content

--- customer-service/src/main/java/lk/ijse/customerservice/service/impl/CustomerServiceImpl.java
+++ customer-service/src/main/java/lk/ijse/customerservice/service/impl/CustomerServiceImpl.java
@@ -2,7 +2,14 @@ package lk.ijse.customerservice.service.impl;
 
 import lk.ijse.customerservice.dto.CustomDTO;
 import lk.ijse.customerservice.dto.CustomerDTO;
+import lk.ijse.customerservice.entity.Customer;
+import lk.ijse.customerservice.repo.CustomerRepo;
 import lk.ijse.customerservice.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
@@ -14,39 +21,60 @@ import java.util.ArrayList;
  * @Time : 4:13 PM
  * @since : 0.1.0
  **/
+
+@Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepo repo;
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public void saveCustomer(CustomerDTO dto) {
-
+        if (repo.existsById(dto.getId())) {
+            throw new RuntimeException("Customer Already Exist. Please enter another id..!");
+        }
+        repo.save(mapper.map(dto, Customer.class));
     }
 
     @Override
     public void updateCustomer(CustomerDTO dto) {
-
+        if (!repo.existsById(dto.getId())) {
+            throw new RuntimeException("Customer Not Exist. Please enter Valid id..!");
+        }
+        repo.save(mapper.map(dto, Customer.class));
     }
 
     @Override
     public void deleteCustomer(CustomerDTO dto) {
-
+        if (!repo.existsById(dto.getId())) {
+            throw new RuntimeException("Wrong ID. Please enter Valid id..!");
+        }
+        repo.delete(mapper.map(dto, Customer.class));
     }
 
     @Override
     public CustomerDTO searchCusId(String id) {
-        return null;
+        if (!repo.existsById(id)) {
+            throw new RuntimeException("Wrong ID. Please enter Valid id..!");
+        }
+        return mapper.map(repo.findById(id).get(), CustomerDTO.class);
     }
 
     @Override
     public ArrayList<CustomerDTO> loadAllCustomer() {
-        return null;
+        return mapper.map(repo.findAll(), new TypeToken<ArrayList<Customer>>() {
+        }.getType());
     }
 
     @Override
     public CustomDTO customerIdGenerate() {
-        return null;
+        return new CustomDTO(repo.getLastIndex());
     }
 
     @Override
     public CustomDTO getSumCustomer() {
-        return null;
+        return new CustomDTO(repo.getSumCustomer());
     }
 }
