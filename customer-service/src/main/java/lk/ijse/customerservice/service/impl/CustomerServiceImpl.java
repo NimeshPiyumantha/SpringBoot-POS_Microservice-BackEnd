ab5bcea4bb369803c7d9181bdc07b9827e52a34b
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 16:14:09 +0530

    Update Content

---
+++ customer-service/src/main/java/lk/ijse/customerservice/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,14 @@
+package lk.ijse.customerservice.service.impl;
+
+import lk.ijse.customerservice.service.CustomerService;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @PACKAGE_NAME : lk.ijse.customerservice.service.impl
+ * @PROJECT_NAME : SpringBoot-POS_Microservice-BackEnd
+ * @Date : 5/24/2023
+ * @Time : 4:13 PM
+ * @since : 0.1.0
+ **/
+public class CustomerServiceImpl implements CustomerService {
+}
