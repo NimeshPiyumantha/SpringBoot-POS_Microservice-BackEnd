3f95fd115b60321bcd091098fcb83677e2be7e28
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 15:34:11 +0530

    Update ApiGatewayApplication class

--- api-gateway/src/main/java/lk/ijse/ApiGatewayApplication.java
+++ api-gateway/src/main/java/lk/ijse/ApiGatewayApplication.java
@@ -1,5 +1,10 @@
 package lk.ijse;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @author : Nimesh Piyumantha
  * @PACKAGE_NAME : lk.ijse
@@ -8,5 +13,12 @@ package lk.ijse;
  * @Time : 3:30 PM
  * @since : 0.1.0
  **/
+@SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
 public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
 }
