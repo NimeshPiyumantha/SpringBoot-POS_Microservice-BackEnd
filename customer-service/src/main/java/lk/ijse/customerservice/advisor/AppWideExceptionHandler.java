dd648500eefa2282e69eed7430741e3d0c2f4376
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 16:04:56 +0530

    Update Content

--- customer-service/src/main/java/lk/ijse/customerservice/advisor/AppWideExceptionHandler.java
+++ customer-service/src/main/java/lk/ijse/customerservice/advisor/AppWideExceptionHandler.java
@@ -22,17 +22,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @CrossOrigin
 public class AppWideExceptionHandler {
     @ExceptionHandler({Exception.class})
-    public ResponseEntity handleException(Exception ex) {
-        return new ResponseEntity(new Response("Error",ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Response> handleException(Exception ex) {
+        return new ResponseEntity<>(new Response("Error",ex.getMessage(),null), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler({NotFoundException.class})
-    public ResponseEntity handleNotFoundException(NotFoundException ex) {
-        return new ResponseEntity(new Response("Error",ex.getMessage()), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Response> handleNotFoundException(NotFoundException ex) {
+        return new ResponseEntity<>(new Response("Error",ex.getMessage(),null), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({ValidationException.class})
-    public ResponseEntity handleValidationException(ValidationException ex) {
-        return new ResponseEntity(new Response("Error",ex.getMessage()), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Response> handleValidationException(ValidationException ex) {
+        return new ResponseEntity<>(new Response("Error",ex.getMessage(),null), HttpStatus.NOT_FOUND);
     }
 }
