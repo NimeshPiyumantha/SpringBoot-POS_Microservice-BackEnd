207948e84595231d8b0bf7de2e622b6fea4eceb3
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 16:10:24 +0530

    Update Content

--- customer-service/src/main/java/lk/ijse/customerservice/dto/CustomerDTO.java
+++ customer-service/src/main/java/lk/ijse/customerservice/dto/CustomerDTO.java
@@ -1,5 +1,10 @@
 package lk.ijse.customerservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  * @author : Nimesh Piyumantha
  * @PACKAGE_NAME : lk.ijse.customerservice.dto
@@ -8,5 +13,13 @@ package lk.ijse.customerservice.dto;
  * @Time : 4:09 PM
  * @since : 0.1.0
  **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class CustomerDTO {
+    private String id;
+    private String name;
+    private String address;
+    private double salary;
 }
