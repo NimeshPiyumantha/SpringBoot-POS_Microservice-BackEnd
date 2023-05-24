022f06bb4173a16159f94ec9d1a9e4aca8f8ca0c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 15:42:24 +0530

    Update Content

---
+++ config-server/src/main/java/lk/ijse/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package lk.ijse.configserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @PACKAGE_NAME : lk.ijse.configserver
+ * @PROJECT_NAME : SpringBoot-POS_Microservice-BackEnd
+ * @Date : 5/24/2023
+ * @Time : 3:42 PM
+ * @since : 0.1.0
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+}
