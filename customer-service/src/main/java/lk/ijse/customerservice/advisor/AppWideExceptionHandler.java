222f8803d1196986bd0d88aabb04bdb8d1399035
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 15:57:09 +0530

    Add Advisor

---
+++ customer-service/src/main/java/lk/ijse/customerservice/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,17 @@
+package lk.ijse.customerservice.advisor;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @PACKAGE_NAME : lk.ijse.customerservice.advisor
+ * @PROJECT_NAME : SpringBoot-POS_Microservice-BackEnd
+ * @Date : 5/24/2023
+ * @Time : 3:56 PM
+ * @since : 0.1.0
+ **/
+@RestControllerAdvice
+@CrossOrigin
+public class AppWideExceptionHandler {
+}
